8eb789557847d316bbbf75a68354ce6d6a264aba
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2024-08-12 11:15:00 -0600

    Fix logic

--- world/apbot/__init__.py
+++ world/apbot/__init__.py
@@ -5,12 +5,6 @@ from .Items import APBotItem
 from .Locations import APBotLocation
 from .Options import APBotOptions
 
-class APBotWeb(WebWorld):
-    tutorials = []
-    theme = "ice"
-
-HUB_NAME = "Menu"
-
 # Junk Item
 JUNK_CODE_OFFSET = 0x000000
 JUNK_ITEM_NAME = "APBot Junk"
@@ -24,6 +18,10 @@ KEY_ITEM_OFFSET = 0x020000
 
 # Chest ID offset
 CHEST_ITEM_OFFSET = 0x030000
+
+class APBotWeb(WebWorld):
+    tutorials = []
+    theme = "ice"
 
 class APBot(World):
     """
@@ -49,58 +47,61 @@ class APBot(World):
         min_chests_per_region = self.options.min_chests_per_region
         max_chests_per_region = self.options.max_chests_per_region
 
-
         if min_chests_per_region > max_chests_per_region:
             raise ValueError("min_chests_per_region must be less than or equal to max_chests_per_region")
 
         itempool = []
 
+        # Menu region
+        menu = Region("Menu", self.player, self.multiworld)
+
         # Create Hub
-        hub = Region(HUB_NAME, self.player, self.multiworld)
+        hub = Region("Hub", self.player, self.multiworld)
 
         # Create Single Chest that contains the starting location
-        starting_chest = APBotLocation(self.player, "Hub Free Chest", JUNK_CODE_OFFSET, hub)
+        starting_chest = APBotLocation(self.player, "Hub Free Chest", CHEST_ITEM_OFFSET, hub)
         hub.locations.append(starting_chest)
 
-        # + 1 for the starting location
         total_junk_items = 0
         for region_num in range(num_regions):
-            # 0x00__ is reserved for the junk item id & the starting location id
-            real_region = region_num + 1
+            region_display_num = region_num + 1
             # Create Region
-            region_name = f"Region {real_region}"
+            region_name = f"Region {region_display_num}"
             region_obj = Region(region_name, self.player, self.multiworld)
 
             # Create Key for this region
-            key_name = f"Key {real_region}"
-            key_code = KEY_ITEM_OFFSET + real_region
-            item = APBotItem(key_name, ItemClassification.progression, key_code, self.player)
-            itempool.append(item)
+            key_name = f"Key {region_display_num}"
+            key_code = KEY_ITEM_OFFSET + region_display_num
+            key_item = APBotItem(key_name, ItemClassification.progression, key_code, self.player)
+            itempool.append(key_item)
             self.item_table[key_name] = {
                 "classification": ItemClassification.progression,
                 "code": key_code,
             }
             self.item_name_to_id[key_name] = key_code
 
-            # Link this region to Hub + 
-            # Rule that the key is required to access the region
-            hub.connect(region_obj, f"Region {real_region} Entrance", lambda state: state.has(key_name, self.player))
-
             num_chests = self.random.randint(min_chests_per_region, max_chests_per_region)
             total_junk_items += num_chests - 1
 
             for chest_num in range(num_chests):
                 real_chest = chest_num + 1
-                chest_name = f"Chest {real_region}-{real_chest}"
-                # First byte = region num, second byte = chest num
-                # This guarantees that each chest has a unique ID
-                chest_id = CHEST_ITEM_OFFSET + (real_region << 8) + real_chest
+                chest_name = f"Chest {region_display_num}-{real_chest}"
+                chest_code = CHEST_ITEM_OFFSET + (region_display_num << 8) + real_chest
 
-                self.location_name_to_id[chest_name] = chest_id
-                location = APBotLocation(self.player, chest_name, chest_id, region_obj)
+                self.location_name_to_id[chest_name] = chest_code
+                location = APBotLocation(self.player, chest_name, chest_code, region_obj)
                 region_obj.locations.append(location)
 
             self.multiworld.regions.append(region_obj)
+
+            # Link this region to Hub &
+            # Rule that the key is required to access the region
+            # Wtf is this referencing bs, Python??? I have to use a default argument to hold on to the correct value????
+            def rule(state, key_name=key_name):
+                # print(f"Checking for {key_name} in {state}")
+                return state.has(key_name, self.player)
+            # print(f"Connecting {hub.name} to {region_obj.name} with key rule checking for {key_name}")
+            hub.connect(region_obj, None, rule)
 
         # Add Goal items
         goal_item = APBotItem(GOAL_ITEM_NAME, ItemClassification.progression, GOAL_ITEM_OFFSET, self.player)
@@ -117,19 +118,29 @@ class APBot(World):
             GOAL_ITEM_NAME: self.options.num_goal_items,
         }, self.player)
 
-
-        # Add junk items
+        # Add Junk items
         self.item_table[JUNK_ITEM_NAME] = {
             "classification": ItemClassification.filler,
             "code": JUNK_CODE_OFFSET,
         }
         self.item_name_to_id[JUNK_ITEM_NAME] = JUNK_CODE_OFFSET
-        # Create num_chests - 1 junk items
-        item = APBotItem(JUNK_ITEM_NAME, ItemClassification.filler, JUNK_CODE_OFFSET, self.player)
+        junk_item = APBotItem(JUNK_ITEM_NAME, ItemClassification.filler, JUNK_CODE_OFFSET, self.player)
         for junk_num in range(total_junk_items - self.options.num_goal_items):
-            itempool.append(item)
+            itempool.append(junk_item)
 
+        self.multiworld.regions.append(menu)
         self.multiworld.regions.append(hub)
+        menu.connect(hub)
+
+        # print(self.item_name_to_id)
+        # print(self.location_name_to_id)
+        # print(self.item_table)
+        # print(itempool)
+        for r in self.multiworld.regions:
+            print(f"{r.name} entrances:", [e for e in r.entrances])
+            print(f"{r.name} exits:", [ex for ex in r.exits])
+            print(f"{r.name} locations:", [loc for loc in r.locations])
+
         self.multiworld.itempool += itempool
 
     # Create Hub -> Regions entrances
